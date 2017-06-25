c6f38f8dd34294b3fdd3546b2bcdf9701c41e1b9
JohnnyBravo75 <tomek_29@gmx.de>
2017-06-25 21:24:28 +0200

    -CHG: ReflectonUril:  Support for Dictionary

--- TwinFinder/Base/Utils/ReflectionUtil.cs
+++ TwinFinder/Base/Utils/ReflectionUtil.cs
@@ -1,9 +1,15 @@
-﻿namespace TwinFinder.Base.Utils
+﻿using System.Collections.Generic;
+
+namespace TwinFinder.Base.Utils
 {
     public class ReflectionUtil
     {
         public static object GetPropertyValue(object obj, string propertyName)
         {
+            if (obj is IDictionary<string, object>)
+            {
+                return (obj as IDictionary<string, object>)[propertyName];
+            }
             return obj.GetType().GetProperty(propertyName).GetValue(obj, null);
         }
     }
