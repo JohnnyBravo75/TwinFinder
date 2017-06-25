c4df23f0ae46e2bf00d740da1ef45555d31dd5b9
JohnnyBravo75 <tomek_29@gmx.de>
2017-06-25 21:37:11 +0200

    -ADD: Fixed checkin

---
+++ TwinFinder.Webservice/Authentication/AuthenticationToken.cs
@@ -0,0 +1,70 @@
+﻿namespace TwinFinder.Webservice
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+#if !SILVERLIGHT
+    [Serializable]
+#endif
+    public class AuthenticationToken
+    {
+        public AuthenticationToken()
+        {
+            this.CreationDateUtc = DateTime.UtcNow;
+            this.ClientId = Guid.NewGuid();
+        }
+
+        public AuthenticationToken(string userName, byte[] passwordHash, string data = null)
+        {
+            this.UserName = userName;
+            this.Data = data;
+            this.PasswordHash = passwordHash;
+            this.CreationDateUtc = DateTime.UtcNow;
+            this.ClientId = Guid.NewGuid();
+        }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public DateTime CreationDateUtc { get; set; }
+
+        [DataMember]
+        public string Data { get; set; }
+
+        [DataMember]
+        public byte[] PasswordHash { get; set; }
+
+        [DataMember]
+        public Guid ClientId { get; set; }
+
+        public bool Equals(AuthenticationToken other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (this.UserName == other.UserName &&
+                this.PasswordHash == other.PasswordHash &&
+                this.ClientId == other.ClientId &&
+                this.CreationDateUtc == other.CreationDateUtc)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsExpired(int expirationMinutes)
+        {
+            if (expirationMinutes <= 0)
+            {
+                return false;
+            }
+
+            return Math.Abs((DateTime.UtcNow - this.CreationDateUtc).TotalMinutes) > expirationMinutes;
+        }
+    }
+}
